67bb954b5a8a392129d04738b12f4d6a1781fd5f
bhupendrasingh26 <bhupendrasingh26@gmail.com>
2017-05-11 19:09:49 +0530

    Configuration Updates

--- src/main/java/com/backbase/atmlocator/config/AtmLocatorWebSecurityConfig.java
+++ src/main/java/com/backbase/atmlocator/config/AtmLocatorWebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.backbase.atmlocator.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,15 +11,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class AtmLocatorWebSecurityConfig extends WebSecurityConfigurerAdapter {
+  
+  @Value("${defaultUser}")
+  private String defaultUserName;
+  
+  @Value("${defaultPassword}")
+  private String defaultPassword;
+  
+  @Value("${role}")
+  private String role;
+  
+  @Value("${contextURI}")
+  private String context;
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("admin").password("pass123").roles("USER");
+    auth.inMemoryAuthentication().withUser(defaultUserName).password(defaultPassword).roles(role);
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/atmlocator").permitAll().anyRequest().authenticated().and().httpBasic();
+    http.authorizeRequests().antMatchers(context).permitAll().anyRequest().authenticated().and().httpBasic();
   }
 
 }
